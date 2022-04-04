5d902f09e6003b037d2171ab236d5daa7947ccbd
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-04-04 20:39:54 +0200

    pico: deploy refactor, support for arguments

--- rp2040/deploy.py
+++ rp2040/deploy.py
@@ -1,11 +1,12 @@
 from sys import argv
+import time
 import bmpParser
 import os
 import mpy_cross
 import subprocess
 import re as regex
+import shutil
 
-COM_PORT = 'COM3'
 
 ignoredFiles = [
     'deploy.py',
@@ -13,7 +14,37 @@ ignoredFiles = [
 ]
 
 
-if len(argv) == 1 or not argv[1] == 'run':
+switches = {
+    'COMPILE': False,
+    'RUN': False,
+    'COM': 'COM3',
+    'OLVL': 3,
+}
+
+
+def parseArgs():
+    global switches
+
+    if '-c' in argv:
+        switches['COMPILE'] = True
+
+    if '-run' in argv:
+        switches['RUN'] = True
+
+    matchOLevel = regex.compile(r'-O(\d)')
+    matchCOM = regex.compile(r'-COM=([^\s]+)')
+
+    for arg in argv:
+        if matchOLevel.match(arg):
+            switches['OLVL'] = matchOLevel.match(arg).group(1)
+
+        if matchCOM.match(arg):
+            switches['COM'] = matchCOM.match(arg).group(1)
+
+    print(switches)
+
+
+def prepOutDir():
     # create 'out' directory
     os.makedirs('out', exist_ok=True)
 
@@ -21,18 +52,36 @@ if len(argv) == 1 or not argv[1] == 'run':
     for file in os.listdir('out'):
         os.remove('out/' + file)
 
-    # convert graphics to JSON
-    bmpParser.convertToJson('out/graphics.json', 'graphics/')
+
+def compileModules():
+    global ignoredFiles, switches
 
     # compile modules to '.mpy'
     for file in os.listdir('.'):
         if file.endswith('.py') and file not in ignoredFiles:
-            mpy_cross.run('-O2', '-o', 'out/' + file[:-3] + '.mpy', file)
+            mpy_cross.run('-O' + switches['OLVL'],
+                          '-o', 'out/' + file[:-3] + '.mpy', file)
 
+
+def copyModules():
+    # copy '.py' files to 'out' dir
+    for file in os.listdir('.'):
+        if file.endswith('.py') and file not in ignoredFiles:
+            shutil.copy(file, 'out/')
+
+
+def convertGraphics():
+    # convert graphics to JSON
+    bmpParser.convertToJson('out/graphics.json', 'graphics/')
+
+
+def clearFilesOnDevice():
+    global switches
     # clear all files on device
     matcher = regex.compile(r'(\d+) ([^\s]+)')
 
-    remote = subprocess.run(['mpremote',  'ls'], stdout=subprocess.PIPE)
+    remote = subprocess.run(
+        ['mpremote', 'connect', switches['COM'], 'ls'], stdout=subprocess.PIPE)
     out = remote.stdout.decode('utf-8').split('\r\n')
 
     for f in out:
@@ -41,18 +90,42 @@ if len(argv) == 1 or not argv[1] == 'run':
         if match and not match.group(2) == 'lib/':
             if not match.group(2).endswith('/'):
                 subprocess.run(
-                    ['mpremote', 'connect', COM_PORT, 'rm', match.group(2)])
+                    ['mpremote', 'connect', switches['COM'], 'rm', match.group(2)])
 
+
+def copyFilesToDevice():
+    global switches
     # copy new files to device
     for file in os.listdir('out'):
-        subprocess.run(['mpremote', 'connect', COM_PORT,
+        subprocess.run(['mpremote', 'connect', switches['COM'],
                        'cp', 'out/' + file, ':'])
 
 
-# run 'main.py'
-try:
-    subprocess.run(['mpremote', 'connect', COM_PORT,
-                   'run', 'main.py'], shell=True)
-except KeyboardInterrupt:
-    print('Stopping...')
-    exit()
+def armDevice():
+    subprocess.run(
+        ['mpremote', 'connect', switches['COM'], 'cp', 'main.py', ':'])
+    time.sleep(2)
+    subprocess.run(['mpremote', 'connect', switches['COM'], 'reset'])
+
+
+parseArgs()
+
+if switches['RUN']:
+    try:
+        subprocess.run(['mpremote', 'connect', switches['COM'],
+                        'run', 'main.py'], shell=True)
+    except KeyboardInterrupt:
+        print('Stopping...')
+        exit()
+else:
+    prepOutDir()
+    convertGraphics()
+
+    if switches['COMPILE']:
+        compileModules()
+    else:
+        copyModules()
+
+    clearFilesOnDevice()
+    copyFilesToDevice()
+    armDevice()
