9effa8359d2fec8d1d8cc8b2f163f74e101c511e
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-04-11 01:58:33 +0200

    esp8266: major refactor, support for flash mode

--- esp8266/main.py
+++ esp8266/main.py
@@ -1,97 +1,23 @@
 from machine import UART
-import struct
 import time
-import math
 import network
 import time
+from shared.opcodes import COMM_POWER, COMM_RESET, GET_SENSORS, GET_WIFI, SENSOR_DATA, WIFI_DATA
+from shared.packets import createRequest, packDataAuto, unpackData
+from shared.uart import readLine
+from shared.utils import loadJSON
 import ubinascii
 from umqtt.simple import MQTTClient
 import machine
-import json
 
 
-TEST_DATA = 00
-SENSOR_DATA = 11
-CONFIG_DATA = 12
-WIFI_DATA = 13
+def connectToWifi(wlan, name, password):
+    wlan.active(True)
 
-GET_CONFIG = 1
-UPDATE_CONFIG = 2
-GET_SENSORS = 3
-GET_WIFI = 4
-
-COMM_POWER = 50
-COMM_RESET = 51
-
-PING = 66
-PONG = 99
-PACKET_STOP = b'\0m]X]X]'
-
-
-def log10(number):
-    return math.log(number) / math.log(10)
-
-
-def readLine(uart: UART):
-    firstLoop = True
-    buffer = b''
-
-    while True:
-        packet: bytes | None = uart.read(1)
-        if packet == None:
-            if firstLoop:
-                return False
-
-            firstLoop = False
-            continue
-
-        buffer += packet
-        if not buffer.endswith(PACKET_STOP):
-            continue
-
-        return buffer
-
-
-def loadConfig():
-    file = open('settings.json', 'r', encoding='utf-8')
-    data: dict[str, dict] = json.load(file)  # type: ignore
-    file.close()
-
-    return data
-
-
-def packData(opcode: int, packetFormat: str, *data):
-    HEADER_SIZE = len(packetFormat) + 2
-    HEADER_SIZE += math.floor(log10(HEADER_SIZE)) + 1
-    packetFormat = 'B{}s'.format(HEADER_SIZE) + packetFormat
-
-    print(packetFormat)
-    header = bytearray(packetFormat, 'utf-8')
-    packet = struct.pack(packetFormat, opcode, header, *data)
-
-    return packet + PACKET_STOP
-
-
-def unpackData(packet):
-    # print(packet)
-    opcode = packet[0]
-    headerSize = int(packet[2:packet.find(b's')])
-    header = packet[1:headerSize + 1].decode()
-    data = struct.unpack(header, packet[:-len(PACKET_STOP)])
-
-    # print('Opcode', opcode)
-    # print('Header Size', headerSize)
-    # print('Header', header)
-    # print('Data', data)
-
-    return data
-
-
-def createRequest(request):
-    packetFormat = 'B3s'
-    header = bytearray(packetFormat)
-
-    return struct.pack(packetFormat, request, header) + PACKET_STOP
+    if not wlan.isconnected():
+        wlan.connect(name, password)
+        while not wlan.isconnected():
+            pass
 
 
 def getSignalPower(wlan):
@@ -119,63 +45,66 @@ def translateSignalPower(power: int):
         return 1
 
 
-uart = UART(0, baudrate=38400, timeout=5000, rxbuf=2048)
-wlan = network.WLAN(network.STA_IF)
-client_id = ubinascii.hexlify(machine.unique_id())
+def main():
+    uart = UART(0, baudrate=38400, timeout=5000, rxbuf=2048)
+    wlan = network.WLAN(network.STA_IF)
+    client_id = ubinascii.hexlify(machine.unique_id())
+
+    settings = loadJSON('settings.json')
+    connectToWifi(wlan, settings['wifi']['name'], settings['wifi']['password'])
+    mqtt = MQTTClient(client_id, settings['mqtt']['ip'], user=settings['mqtt']['login'], password=settings['mqtt']['password'], keepalive=60)
 
 
-def sub_cb(topic, msg):
-    print((topic, msg))
+    def sub_cb(topic, msg):
+        print((topic, msg))
 
-    if msg == b'power':
-        uart.write(createRequest(COMM_POWER))
-    elif msg == b'reset':
-        uart.write(createRequest(COMM_RESET))
-    elif msg == b'getSensors':
-        uart.write(createRequest(GET_SENSORS))
+        if msg == b'power':
+            uart.write(createRequest(COMM_POWER))
+        elif msg == b'reset':
+            uart.write(createRequest(COMM_RESET))
+        elif msg == b'getSensors':
+            uart.write(createRequest(GET_SENSORS))
 
 
-counter = 0
-MQTT_BACKOFF = 0
-LOOP_TIME = 1
+    mqtt.set_callback(sub_cb)
+    mqtt.connect()
+    mqtt.subscribe(b"pico_in")
 
-settings = loadConfig()
-uart.read()
-c = MQTTClient(client_id, settings['mqtt']['ip'], user=settings['mqtt']
-               ['login'], password=settings['mqtt']['password'], keepalive=60)
-c.set_callback(sub_cb)
-c.connect()
-c.subscribe(b"pico_in")
-# print(getSignalPower(wlan))
+    MQTT_BACKOFF = 0
+    LOOP_TIME = 1
 
-while True:
-    while uart.any() > 0:
-        packet = readLine(uart)
-        if not packet:
-            continue
 
-        try:
-            data = unpackData(packet)
+    while True:
+        while uart.any() > 0:
+            packet = readLine(uart)
+            if not packet:
+                continue
 
-            if data[0] == GET_WIFI:
-                signal = getSignalPower(wlan)
-                translated = translateSignalPower(signal)
+            try:
+                data = unpackData(packet)
 
-                print('Signal:', signal, 'Translated:', translated)
-                packet = packData(WIFI_DATA, 'i', translated)
-                uart.write(packet)
-            elif data[0] == SENSOR_DATA:
-                # print(packet)
-                c.publish(b'pico_out', packet)
+                if data[0] == GET_WIFI:
+                    signal = getSignalPower(wlan)
+                    translated = translateSignalPower(signal)
 
-        except Exception as e:
-            print(e)
-            print('Corrupted packet:', packet)
+                    print('Signal:', signal, 'Translated:', translated)
+                    packet = packDataAuto(WIFI_DATA, translated)
+                    uart.write(packet)
+                elif data[0] == SENSOR_DATA:
+                    mqtt.publish(b'pico_out', packet)
 
-    c.check_msg()
+            except Exception as e:
+                print(e)
+                print('Corrupted packet:', packet)
 
-    if MQTT_BACKOFF == 0:
-        c.ping()
+        mqtt.check_msg()
 
-    MQTT_BACKOFF = (MQTT_BACKOFF + 1) % ((1000 // LOOP_TIME) * 5)
-    time.sleep_ms(LOOP_TIME)
+        if MQTT_BACKOFF == 0:
+            mqtt.ping()
+
+        MQTT_BACKOFF = (MQTT_BACKOFF + 1) % ((1000 // LOOP_TIME) * 5)
+        time.sleep_ms(LOOP_TIME)
+
+
+if not FLASH_MODE:
+    main()
