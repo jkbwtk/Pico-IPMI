da5aabfd8621c0c2d0ee1e47dad9da0cca319ea8
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-04-10 23:09:06 +0200

    esp8266: implementeded flash mode

--- esp8266/boot.py
+++ esp8266/boot.py
@@ -1,12 +1,21 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
-import machine
+from machine import Pin
 import uos
 import webrepl
 import gc
 from network import WLAN, AP_IF
 import esp
 esp.osdebug(None)
-uos.dupterm(None, 1)
+
+button = Pin(16, Pin.IN)
+led = Pin(2, Pin.OUT, value=1)
+
+
+if button.value() == 0:
+  led.value(0)
+else:
+  led.value(1)
+  uos.dupterm(None, 1)
 
 # disable access point
 ap = WLAN(AP_IF)
