0db7237e842c132d8bf979852adc0730ae18e5bc
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-04-15 02:38:18 +0200

    driver: experimental UART helper class

--- driver/main.py
+++ driver/main.py
@@ -5,7 +5,7 @@ import winreg
 import serial
 from shared.opcodes import GET_SENSORS, GET_SYSINFO, PING, PONG, SENSOR_DATA, SYSINFO_DATA, SYSINFO_OK, UPDATE_SYSINFO
 from shared.packets import createRequest, packDataAuto, unpackData
-from shared.uart import readLine
+from shared.uart import UART
 from shared.utils import getHash
 
 from driver.sensors import getSensors, getSysInfo
@@ -20,19 +20,17 @@ LOOP_TIME = 1  # ms
 reg = winreg.ConnectRegistry(None, winreg.HKEY_CURRENT_USER)
 key = winreg.OpenKey(reg, 'SOFTWARE\\HWiNFO64\\VSB')
 
-uart = serial.Serial('COM5', baudrate=38400, timeout=0.01)
+uart =  UART(serial.Serial('COM5', baudrate=38400, timeout=0.01))
 
 sysInfo = getSysInfo()
 hash = getHash(sysInfo)
 
-
-uart.read_all()  # flush system buffer
 uart.write(packDataAuto(UPDATE_SYSINFO, hash))
 
 while True:
-    while uart.in_waiting > 0:
+    while uart.uart.in_waiting > 0:
         try:
-            packet = readLine(uart)
+            packet = uart.readLine()
             if not packet:
                 continue
 
--- shared/uart.py
+++ shared/uart.py
@@ -1,21 +1,108 @@
+import random
+import string
+import time
 from shared.opcodes import PACKET_STOP
 
 
-def readLine(uart):
-    firstLoop = True
-    buffer = b''
+try:
+    from machine import Timer
+except:
+    import threading
+    import signal
 
-    while True:
-        packet: bytes | None = uart.read(1)
-        if packet == None:
-            if firstLoop:
-                return False
+    TERMINATED = False
 
-            firstLoop = False
-            continue
+    def terminator(signum, frame):
+        global TERMINATED
 
-        buffer += packet
-        if not buffer.endswith(PACKET_STOP):
-            continue
+        print('Terminating')
+        TERMINATED = True
+        raise InterruptedError()
 
-        return buffer
+    signal.signal(signal.SIGTERM, terminator)
+    signal.signal(signal.SIGINT, terminator)
+
+    class Timer(threading.Thread):
+        PERIODIC = 0
+
+        def __init__(self):
+            threading.Thread.__init__(self)
+            self.daemon = False
+            self.loop = threading.Event()
+
+        def init(self, freq, mode, callback):
+            self.interval = 1 / freq
+            self.mode = mode
+            self.callback = callback
+
+            self.start()
+
+        def deinit(self):
+            self.loop.set()
+            self.join()
+
+        def run(self):
+            while not self.loop.wait(self.interval):
+                if TERMINATED:
+                    return self.deinit()
+
+                self.callback(self)
+
+
+class UART:
+    def __init__(self, uart) -> None:
+        self.uart = uart
+
+        self.packetTimer = Timer()
+        self.packetTimer.init(freq=1 / 2, mode=Timer.PERIODIC, callback=self.packetChecker)
+
+        self.pendingPackets = {} # noun: [timestamp, retries, packet]
+
+    def __del__(self):
+        self.packetTimer.deinit()
+
+    def packetChecker(self, timer):
+        now = time.time_ns()
+        items = list(self.pendingPackets.items())
+
+        for noun, packet in items:
+            if now >= packet[0] + 1_000_000_000:
+                if packet[1] >= 5:
+                    print('CONNECTION ERROR!', noun, packet)
+                    self.pendingPackets.pop(noun)
+                    continue
+
+                packet[1] += 1
+                packet[0] = now
+                print(noun, 'Retries:', packet[1])
+                # self.uart.write(packet[2])
+
+    def watchPacket(self, packet: bytes):
+        noun = ''.join(random.choice(string.ascii_letters) for _ in range(12))
+
+        self.pendingPackets[noun] = [time.time_ns(), 0, packet]
+
+    def write(self, packet: bytes, important=False):
+        if important:
+            self.watchPacket(packet)
+
+        self.uart.write(packet)
+
+    def readLine(self):
+        firstLoop = True
+        buffer = b''
+
+        while True:
+            packet: bytes | None = self.uart.read(1)
+            if packet == None:
+                if firstLoop:
+                    return False
+
+                firstLoop = False
+                continue
+
+            buffer += packet
+            if not buffer.endswith(PACKET_STOP):
+                continue
+
+            return buffer
