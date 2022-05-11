c40891ce4e951961c6467118e1e58e471f838771
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-05-11 23:35:42 +0200

    pico: loop rewrite, experimental features, routing, new packet format

--- rp2040/displayRoutine.py
+++ rp2040/displayRoutine.py
@@ -1,22 +1,26 @@
 import time
 from ssd1306 import SSD1306_I2C
 import framebuf
-from machine import I2C, Pin, Timer, freq
+from machine import I2C, Pin, Timer
 import bmpParser as bmp
 
 
-SCREEN_UPDATED = False
+SCREEN_ACTIVE = False
 DISPLAY_STATE = -1
 COUNTER = 0
 
 WIFI_SIGNAL = 0
+MQTT_STATUS = 0
 SENSORS = {}
+
+SENSOR_OFFSET = 0
 
 
 # freq(260_000_000)
 # print('Changed cpu freq to {} Mhz'.format(freq() // 1_000_000))
-i2c = I2C(0, scl=Pin(17), sda=Pin(16), freq=200000)
+i2c = I2C(0, scl=Pin(21), sda=Pin(20), freq=400_000)
 display = SSD1306_I2C(128, 64, i2c)
+timer = Timer()
 
 display.fill(0)
 display.contrast(0)
@@ -27,6 +31,38 @@ t2 = time.ticks_ms()
 print('Graphics loaded in:', time.ticks_diff(t2, t1), 'ms')
 
 custom = bytearray([])
+
+
+def forceRefresh(fn):
+    def wrapper(*args, **kwargs):
+        fn(*args, **kwargs)
+        refresh(-1)
+
+    return wrapper
+
+
+def toggle():
+    global SCREEN_ACTIVE, timer
+
+    SCREEN_ACTIVE = not SCREEN_ACTIVE
+
+    if not SCREEN_ACTIVE:
+        timer.deinit()
+        display.fill(0)
+        display.show()
+    else:
+        refresh(-1)
+        timer.init(freq=5, mode=Timer.PERIODIC, callback=refresh)
+
+
+def powerOff():
+    if SCREEN_ACTIVE:
+        toggle()
+
+
+def powerOn():
+    if not SCREEN_ACTIVE:
+        toggle()
 
 
 def horizCenter(text: str, y: int, color: int):
@@ -46,7 +82,11 @@ def centeredImage(image: tuple):
 def updateWiFiSignal(level: int):
     global WIFI_SIGNAL, SCREEN_UPDATED
     WIFI_SIGNAL = level % len(graphics.get('wifi'))
-    SCREEN_UPDATED = True
+
+
+def updateMQTTStatus(status: int):
+    global MQTT_STATUS, SCREEN_UPDATED
+    MQTT_STATUS = status % len(graphics.get('mqtt'))
 
 
 def updateSensors(sensors: dict):
@@ -59,6 +99,7 @@ def updateCustomImage(image: bytearray):
     custom = image
 
 
+@forceRefresh
 def changeState(state: int):
     global DISPLAY_STATE
     DISPLAY_STATE = state
@@ -72,6 +113,9 @@ def stateIdle():
 def stateConnecting():
     global COUNTER
 
+    drawWiFi()
+    drawMQTT()
+
     horizCenter('Syncing', 17, 1)
     horizCenter('With Host', 27, 1)
     display.text('.' * (COUNTER % 4), 52, 37, 1)
@@ -79,6 +123,9 @@ def stateConnecting():
 
 def stateConnectionLost():
     display.fill(0)
+
+    drawWiFi()
+    drawMQTT()
 
     if COUNTER % 10 > 4:
         display.fill_rect(28, 5, 8, 40, 1)
@@ -105,44 +152,108 @@ def drawWiFi():
     display.blit(frame, 120, 0, 0)
 
 
+def drawMQTT():
+    mqtt = graphics.get('mqtt')[MQTT_STATUS]
+    frame = framebuf.FrameBuffer(
+        bytearray(mqtt[2]), mqtt[0], mqtt[1], framebuf.MONO_HLSB)
+    display.blit(frame, 120, 10, 0)
+
+
+def drawBar(val: int, minn: int, maxx: int, x: int, y: int, width: int, height: int, orientation: int, color: int):
+    if val < minn:
+        return
+
+    if val > maxx:
+        val = maxx
+
+    val = (val - minn) / (maxx - minn)
+
+    if orientation == 0:
+        val = int(val * width)
+        display.fill_rect(x, y, val, height, color)
+    elif orientation == 1:
+        val = int(val * height)
+        display.fill_rect(x, y, width, val, color)
+    elif orientation == 2:
+        val = int(val * width)
+        display.fill_rect(x + (width - val), y, val, height, color)
+    elif orientation == 3:
+        val = int(val * height)
+        display.fill_rect(x, y + (height - val), width, val, color)
+
+
 def drawSensors():
-    display.text('CPU: {}%'.format(SENSORS.get('cpu_usage', -1)), 0, 0, 1)
-    display.text('CPU: {}C'.format(SENSORS.get('cpu_temp', -1)), 0, 9, 1)
-    display.text('CPU: {}W'.format(SENSORS.get('cpu_power', -1)), 0, 18, 1)
-    display.text('RAM: {}MB'.format(SENSORS.get('ram_used', -1)), 0, 27, 1)
-    display.text('RAM: {}MHz'.format(SENSORS.get('ram_freq', -1)), 0, 36, 1)
-    display.text('GPU: {}%'.format(SENSORS.get('gpu0_usage', -1)), 0, 45, 1)
-    display.text('GPU: {}C'.format(SENSORS.get('gpu0_temp', -1)), 0, 54, 1)
+    global SENSOR_OFFSET
+
+    if SENSORS == {}:
+        return
+
+    pages = 2
+
+    if SENSOR_OFFSET % pages == 1:
+        cpus = len(SENSORS['cpus'])
+        barWidth = int(120 / cpus)
+
+        horizCenter(f'{cpus} CPUs', 0, 1)
+        horizCenter(f'Total: {SENSORS.get("cpu_usage", -1):.0f}%', 10, 1)
+
+        for i, v in enumerate(SENSORS['cpus']):
+            drawBar(v, 0, 100, i * barWidth, 20, barWidth, 44, 3, 1)
+
+    elif SENSOR_OFFSET % pages == 0:
+        display.text(f'CPU: {SENSORS.get("cpu_usage", -1)}%', 0, 0, 1)
+        display.text(f'CPU: {SENSORS.get("cpu_temp", -1)}C', 0, 9, 1)
+        display.text(f'CPU: {SENSORS.get("cpu_power", -1)}W', 0, 18, 1)
+        display.text(f'RAM: {SENSORS.get("ram_used", -1)}MB', 0, 27, 1)
+        display.text(f'RAM: {SENSORS.get("ram_freq", -1)}MHz', 0, 36, 1)
+
+        if (SENSORS['gpus'] != 0):
+            display.text(f'GPU: {SENSORS["gpus"][0].get("usage", -1)}%', 0, 45, 1)
+            display.text(f'GPU: {SENSORS["gpus"][0].get("temp", -1)}C', 0, 54, 1)
+        else:
+            display.text(f'FAN: {SENSORS.get("cpu_fan", -1)}RPM'.format(), 0, 45, 1)
+            display.text(f'BATT: {SENSORS.get("batt_level", -1)}%'.format(), 0, 54, 1)
 
 
 def stateWorking():
     drawWiFi()
+    drawMQTT()
     drawSensors()
 
 
+STATE_CONNECTING = 0
+STATE_CONNECTION_LOST = 1
+STATE_WORKING = 2
+STATE_CUSTOM_PICTURE = 50
+STATE_IDLE = -1
+
+
 def refresh(timer):
-    global COUNTER, DISPLAY_STATE
+    global COUNTER, DISPLAY_STATE, SCREEN_ACTIVE
+
+    if not SCREEN_ACTIVE:
+        return
+
     display.fill(0)
 
-    if DISPLAY_STATE == 0:
+    if DISPLAY_STATE == STATE_CONNECTING:
         stateConnecting()
-    elif DISPLAY_STATE == 1:
+    elif DISPLAY_STATE == STATE_CONNECTION_LOST:
         stateConnectionLost()
-    elif DISPLAY_STATE == 2:
+    elif DISPLAY_STATE == STATE_WORKING:
         stateWorking()
-    elif DISPLAY_STATE == 50:
+    elif DISPLAY_STATE == STATE_CUSTOM_PICTURE:
         stateCustomPicture()
     else:
         stateIdle()
 
-    COUNTER = (COUNTER % 65_536) + 1
+    COUNTER = (COUNTER % 65_535) + 1
     display.show()
 
+
+toggle()
 
 t1 = time.ticks_ms()
 refresh(-1)
 t2 = time.ticks_ms()
 print('Frame presented in:', time.ticks_diff(t2, t1), 'ms')
-
-timer = Timer()
-timer.init(freq=5, mode=Timer.PERIODIC, callback=refresh)
--- rp2040/main.py
+++ rp2040/main.py
@@ -1,182 +1,448 @@
 import json
+import math
 import time
-from shared.sensors import parseSensors
-
-from machine import UART, Pin, Timer
-from shared.opcodes import COMM_POWER, COMM_RESET, GET_SENSORS, GET_SYSINFO, GET_WIFI, PONG, SENSOR_DATA, SYSINFO_DATA, SYSINFO_OK, UPDATE_SYSINFO, WIFI_DATA
-from shared.packets import createRequest, packDataAuto, unpackData
-from shared.uart import readLine
-from shared.utils import getHash, loadJSON, saveJSON
-from utime import sleep_ms
 
 import displayRoutine as display
 
+from machine import PWM, UART, Pin, Timer, ADC
+from micropython import const
+from shared.baseClient import genericRXHandler
+from shared.debouncer import debounce
+from shared.opcodes import (
+    AMBIENT_TEMP_DATA, COMM_POWER, COMM_RESET, ENCODER_LEFT, ENCODER_PRESSED, ENCODER_RIGHT, GET_AMBIENT_TEMP,
+    GET_HDD_ACTIVITY, GET_MQTT_STATUS, GET_POWER_STATUS, GET_SENSORS, GET_SYSINFO, GET_WIFI, HDD_ACTIVITY_DATA,
+    MQTT_STATUS_DATA, OK, PING, PONG, POWER_STATUS_DATA, SENSOR_DATA, SYSINFO_DATA, SYSINFO_OK, TEST_DATA,
+    UPDATE_SYSINFO, WIFI_DATA)
+from shared.routes import ESP, HOST, PICO, PRIVATE
+from shared.sensors import parseSensors
+from shared.uart import UART as UartBase
+from shared.utils import (getHash, getUniq, loadJSON, saveJSON, time_ns,
+                          timeDivider)
 
 
-led = Pin(25, Pin.OUT)
-uart0 = UART(0, baudrate=38400, tx=Pin(0), rx=Pin(1), timeout=1000, rxbuf=2048)
-uart1 = UART(1, baudrate=38400, tx=Pin(4), rx=Pin(5), timeout=1000, rxbuf=2048)
-relay1 = Pin(22, Pin.OUT)
-relay2 = Pin(21, Pin.OUT)
-
-relay1.high()
-relay2.high()
-
-
-UART0_HEARTBEAT_COUNTER = 0
-UART0_WORKING_FLAG = False
-UART0_WRITING = False
-UART0_BACKOFF = 0
+picoLED = Pin(25, Pin.OUT)
+powerSwitch = Pin(27, Pin.OUT, value=1)
+resetSwitch = Pin(26, Pin.OUT, value=1)
+powerLED = Pin(16, Pin.IN, Pin.PULL_DOWN)
+hddLED = Pin(17, Pin.IN, Pin.PULL_DOWN)
+buzzer = PWM(Pin(18))
+nextButton = Pin(19, Pin.IN, Pin.PULL_UP)
+tempSensor = ADC(4)
 
 
-UART1_HEARTBEAT_COUNTER = 0
-UART1_WORKING_FLAG = False
-UART1_WRITING = False
-UART1_BACKOFF = 0
+class State:
+    def __init__(self) -> None:
+        self.sysInfo = loadJSON('sysInfo.json')
+        self.hash = getHash(self.sysInfo)
 
+        self.hostConnected = False
+        self.espConnected = False
 
-def fetchSensors(timer):
-    global UART0_WRITING
-    global UART0_HEARTBEAT_COUNTER
-    global UART0_WORKING_FLAG
-    global UART0_BACKOFF
+        self.uartHost = UartHost(self)
+        self.uartESP = UartESP(self)
 
-    if UART0_WORKING_FLAG and not UART0_WRITING:
-        UART0_WRITING = True
-        uart0.write(createRequest(GET_SENSORS))
-        UART0_WRITING = False
+        self.HDDActivity = HDDActivityRecorder()
 
-        UART0_HEARTBEAT_COUNTER += 1
+        self.hostSensorsRequestUniq = None
+        self.espMQTTStatusRequestUniq = None
 
-        if UART0_HEARTBEAT_COUNTER > 5:
-            UART0_WORKING_FLAG = False
-            print('Lost connection with host')
-            display.changeState(1)
-            UART0_BACKOFF = 1
+    def fetchSensors(self):
+        if self.hostSensorsRequestUniq is None:
+            uniq = getUniq()
+            self.hostSensorsRequestUniq = uniq
+            self.route([GET_SENSORS, PICO, HOST, uniq, PRIVATE], important=True)
 
+    def fetchMQTTStatus(self):
+        if self.espConnected:
+            if self.espMQTTStatusRequestUniq is None:
+                uniq = getUniq()
+                self.espMQTTStatusRequestUniq = uniq
+                self.route([GET_MQTT_STATUS, PICO, ESP, uniq, PRIVATE], important=True)
+        else:
+            self.route([GET_MQTT_STATUS, PICO, ESP, getUniq(), PRIVATE], important=False)
 
-def fetchWiFi(timer):
-    global UART1_WRITING
-    global UART1_HEARTBEAT_COUNTER
-    global UART1_WORKING_FLAG
+    def fetchWiFiSignal(self):
+        self.route([GET_WIFI, PICO, ESP, getUniq(), PRIVATE], important=True)
 
-    if UART1_WORKING_FLAG and not UART1_WRITING:
-        UART1_WRITING = True
-        uart1.write(createRequest(GET_WIFI))
-        UART1_WRITING = False
+    def connectHost(self):
+        if not self.hostConnected:
+            print('Connecting to host')
+            self.route([GET_SYSINFO, PICO, HOST, getUniq(), PRIVATE, self.hash])
 
-        UART1_HEARTBEAT_COUNTER += 1
+    def route(self, packet: list, important=False) -> bool:
+        if packet[2] == PICO:
+            return False
 
-        if UART1_HEARTBEAT_COUNTER > 5:
-            UART1_WORKING_FLAG = False
-            print('Lost connection with Wi-Fi module')
-            display.updateWiFiSignal(0)
+        if packet[2] == HOST:
+            self.uartHost.writePacket(packet, important)
+        else:
+            self.uartESP.writePacket(packet, important)
 
+        return True
 
-sysInfo = loadJSON('sysInfo.json')
-hash = getHash(sysInfo)
-
-rawSensorsPacket = b''
-
-LOOP_TIME = 1
-SHBTMR = Timer()
-WIFIHBTMR = Timer()
-
-time.sleep(2)
-SHBTMR.init(freq=1 / 2, mode=Timer.PERIODIC, callback=fetchSensors)
-WIFIHBTMR.init(freq=1 / 10, mode=Timer.PERIODIC, callback=fetchWiFi)
-
-
-while True:
-    while uart0.any() > 0:
-        # print(uart0.any())
-        packet = readLine(uart0)
-        if not packet:
-            continue
-
-        try:
-            data = unpackData(packet)
-
-            if data[0] == SENSOR_DATA:
-                if data[-1] != hash:
-                    uart0.write(packDataAuto(GET_SYSINFO, hash))
-                    UART0_HEARTBEAT_COUNTER = 0
-                else:
-                    display.updateSensors(parseSensors(data, sysInfo))
-                    rawSensorsPacket = packet
-                    UART0_HEARTBEAT_COUNTER = 0
-            elif data[0] == SYSINFO_DATA:
-                sysInfo = json.loads(data[2])
-                saveJSON('sysInfo.json', data[2])
-                hash = getHash(sysInfo)
-                print('Synced sysInfo file')
-                display.changeState(2)
-                UART0_WORKING_FLAG = True
-                UART0_HEARTBEAT_COUNTER = 0
-            elif data[0] == UPDATE_SYSINFO:
-                if data[2] != hash:
-                    UART0_WORKING_FLAG = False
-                    UART0_BACKOFF = 0
-                else:
-                    print('Valid sysInfo')
-                    UART0_WORKING_FLAG = True
-                    UART0_HEARTBEAT_COUNTER = 0
-                    display.changeState(2)
-            elif data[0] == SYSINFO_OK:
-                UART0_WORKING_FLAG = True
+    def handleRX(self, packet):
+        if packet[0] == TEST_DATA:
+            print('Test data:', packet[5])
+        elif packet[0] == PING:
+            self.route([PONG, packet[2], packet[1], packet[3], packet[4]])
+        elif packet[0] == UPDATE_SYSINFO:
+            if packet[5][0] != self.hash:
+                self.uartHost.updateSysInfo()
+            else:
                 print('Valid sysInfo')
-                display.changeState(2)
-                UART0_HEARTBEAT_COUNTER = 0
-            elif data[0] == PONG:
-                UART0_HEARTBEAT_COUNTER = 0
-        except Exception as e:
-            print(e)
-            print('Corrupted packet:', packet)
+        elif packet[0] == SYSINFO_DATA:
+            self.sysInfo = json.loads(packet[5][0])
+            saveJSON('sysInfo.json', self.sysInfo)
+            self.hash = getHash(self.sysInfo)
+            print('Synced sysInfo file')
+            self.uartHost.stateConnected()
+            self.route([UPDATE_SYSINFO, PICO, ESP, getUniq(), PRIVATE, self.hash], important=True)
+        elif packet[0] == SYSINFO_OK:
+            print('Valid sysInfo')
+            self.uartHost.stateConnected()
+        elif packet[0] == SENSOR_DATA:
+            self.hostSensorsRequestUniq = None
+            if packet[5].pop(0) != self.hash:
+                print('Invalid sysInfo hash, resyncing')
+                self.uartHost.updateSysInfo()
+            else:
+                # print(parseSensors(packet[5], self.sysInfo))
+                display.updateSensors(parseSensors(packet[5], self.sysInfo))
+        elif packet[0] == WIFI_DATA:
+            display.updateWiFiSignal(packet[5][0])
+        elif packet[0] == MQTT_STATUS_DATA:
+            self.uartESP.stateWorking()
+            self.espMQTTStatusRequestUniq = None
+            display.updateMQTTStatus(packet[5][0])
+        elif packet[0] == COMM_POWER:
+            powerSwitch.low()
+            time.sleep_ms(100)
+            powerSwitch.high()
 
-    while uart1.any() > 0:
-        # print(uart1.any())
-        packet = readLine(uart1)
-        if not packet:
-            continue
+            self.route([
+                OK,
+                packet[2],
+                packet[1],
+                packet[3],
+                packet[4]
+            ])
+        elif packet[0] == COMM_RESET:
+            resetSwitch.low()
+            time.sleep_ms(200)
+            resetSwitch.high()
 
-        try:
-            data = unpackData(packet)
-            print(data)
+            self.route([
+                OK,
+                packet[2],
+                packet[1],
+                packet[3],
+                packet[4]
+            ])
 
-            if data[0] == WIFI_DATA:
-                display.updateWiFiSignal(data[2])
-                UART1_HEARTBEAT_COUNTER = 0
-                UART1_WORKING_FLAG = True
-            elif data[0] == COMM_POWER:
-                relay1.low()
-                sleep_ms(100)
-                relay1.high()
-            elif data[0] == COMM_RESET:
-                relay2.low()
-                sleep_ms(200)
-                relay2.high()
-            elif data[0] == GET_SENSORS:
-                print(rawSensorsPacket)
-                uart1.write(rawSensorsPacket)
+        elif packet[0] == GET_SYSINFO:
+            if len(packet[5]) > 0 and packet[5][0] == self.hash:
+                self.route([
+                    SYSINFO_OK,
+                    packet[2],
+                    packet[1],
+                    packet[3],
+                    PRIVATE
+                ])
+            else:
+                self.route([
+                    SYSINFO_DATA,
+                    packet[2],
+                    packet[1],
+                    packet[3],
+                    PRIVATE,
+                    json.dumps(self.sysInfo)
+                ])
 
-        except Exception as e:
-            print(e)
-            print('Corrupted packet:', packet)
+        elif packet[0] == GET_POWER_STATUS:
+            self.route([
+                POWER_STATUS_DATA,
+                packet[2],
+                packet[1],
+                packet[3],
+                PRIVATE,
+                powerLED.value()
+            ])
 
-    if not UART0_WORKING_FLAG and UART0_BACKOFF == 0:
-        print('Syncing sysInfo')
-        display.changeState(0)
+        elif packet[0] == GET_HDD_ACTIVITY:
+            self.route([
+                HDD_ACTIVITY_DATA,
+                packet[2],
+                packet[1],
+                packet[3],
+                PRIVATE,
+                self.HDDActivity.activity
+            ])
 
-        UART0_WRITING = True
-        uart0.write(packDataAuto(GET_SYSINFO, hash))
-        UART0_WRITING = False
+        elif packet[0] == GET_AMBIENT_TEMP:
+            self.route([
+                AMBIENT_TEMP_DATA,
+                packet[2],
+                packet[1],
+                packet[3],
+                PRIVATE,
+                readAmbientTemp()
+            ])
 
-    if not UART1_WORKING_FLAG and UART1_BACKOFF == 0:
-        print('Connecting to Wi-Fi module')
-        UART1_WRITING = True
-        uart1.write(createRequest(GET_WIFI))
-        UART1_WRITING = False
+        # if packet[1] == HOST and not self.hostConnected:
+            # self.uartHost.stateWorking()
+        # if packet[1] != HOST and not self.espConnected:
+        #     self.uartESP.stateWorking()
 
-    UART0_BACKOFF = (UART0_BACKOFF + 1) % ((1000 // LOOP_TIME) * 5)
-    UART1_BACKOFF = (UART1_BACKOFF + 1) % ((1000 // LOOP_TIME) * 5)
-    time.sleep_ms(LOOP_TIME)
+
+class UartHost(UartBase):
+    def __init__(self, state: State) -> None:
+        uart = UART(0,  baudrate=57600, tx=Pin(0), rx=Pin(1), timeout=0, rxbuf=2048)
+        self.state = state
+
+        super().__init__(uart)
+
+    @genericRXHandler
+    def handleRX(self, packet: list):
+        if self.state.route(packet):
+            return
+
+        if self.requests.get(packet[3]) is not None:
+            # print('Deleting request:', packet[3])
+            self.requests.pop(packet[3])
+
+        self.state.handleRX(packet)
+
+    def updateSysInfo(self):
+        self.state.hostSensorsRequestUniq = None
+        self.state.hostConnected = False
+        display.changeState(display.STATE_CONNECTING)
+        self.state.connectHost()
+
+    def stateConnected(self):
+        self.state.hostSensorsRequestUniq = None
+        self.state.hostConnected = True
+        display.changeState(display.STATE_WORKING)
+        self.state.fetchSensors()
+
+    def handleFailedRequest(self, request: list) -> None:
+        if not self.state.hostConnected:
+            return
+
+        super().handleFailedRequest(request)
+
+        self.state.hostSensorsRequestUniq = None
+        display.changeState(display.STATE_CONNECTION_LOST)
+        display.refresh(None)
+        time.sleep(5)
+        self.updateSysInfo()
+
+
+class UartESP(UartBase):
+    def __init__(self, state: State) -> None:
+        uart = UART(1, baudrate=57600, tx=Pin(4), rx=Pin(5), timeout=0, rxbuf=2048)
+        self.state = state
+
+        super().__init__(uart)
+
+    @genericRXHandler
+    def handleRX(self, packet: list):
+        if self.state.route(packet):
+            return
+
+        if self.requests.get(packet[3]) is not None:
+            self.requests.pop(packet[3])
+
+        self.state.handleRX(packet)
+
+    def stateWorking(self):
+        self.state.espConnected = True
+        self.state.espMQTTStatusRequestUniq = None
+
+    def handleFailedRequest(self, request: list) -> None:
+        if not self.state.espConnected:
+            return
+
+        super().handleFailedRequest(request)
+
+        self.state.espMQTTStatusRequestUniq = None
+        self.state.espConnected = False
+        display.updateWiFiSignal(0)
+        display.updateMQTTStatus(0)
+
+
+def readAmbientTemp() -> float:
+    val = tempSensor.read_u16() * (3.3 / 65536)
+    temp = 27 - (val - 0.706) / 0.001721
+
+    return temp
+
+
+def setupEncoder(state: State):
+    btn = Pin(11, Pin.IN)
+    left = Pin(12, Pin.IN)
+    right = Pin(13, Pin.IN)
+
+    # @debounce(10)
+    def leftHandler(_):
+        # state.route(Packet(ENCODER_LEFT, PICO, HOST, getUniq(), PRIVATE))
+        display.SENSOR_OFFSET += 1
+
+    @debounce(50)
+    def rightHandler(_):
+        if left.value() == 0:
+            return leftHandler(_)
+
+        # state.route(Packet(ENCODER_RIGHT, PICO, HOST, getUniq(), PRIVATE))
+        display.SENSOR_OFFSET -= 1
+
+    @debounce(300)
+    def btnHandler(_):
+        display.toggle()
+        # state.route(Packet(ENCODER_PRESSED, PICO, HOST, getUniq(), PRIVATE))
+
+    btn.irq(trigger=Pin.IRQ_FALLING, handler=btnHandler.exec)
+    # left.irq(trigger=Pin.IRQ_FALLING, handler=leftHandler.exec)
+    right.irq(trigger=Pin.IRQ_FALLING, handler=rightHandler.exec)
+
+
+right_assist = 0
+count = 0  # Rotate the value of the encoder
+
+
+def setupEncoderV2(state: State):
+    right = Pin(13, Pin.IN)
+    left = Pin(12, Pin.IN)
+    down = Pin(11, Pin.IN)
+    timer = Timer()
+
+    def right_handler(pin):
+        global right_assist
+        right.irq(handler=None)
+        right_assist = 1
+
+    def down_handler(pin):
+        global count
+        down.irq(handler=None)
+        count = 0
+        print("down", count)
+        state.route([ENCODER_PRESSED, PICO, HOST, getUniq(), PRIVATE])
+        down.irq(trigger=Pin.IRQ_FALLING, handler=down_handler)
+
+    def loop(_):
+        global count, right_assist
+
+        if (right_assist == 1):
+            if (left.value() == 1):
+                count = count - 1
+                print("left",  count)
+                state.route([ENCODER_LEFT, PICO, HOST, getUniq(), PRIVATE])
+
+            elif (left.value() == 0):
+                count = count + 1
+                print("right",  count)
+                state.route([ENCODER_RIGHT, PICO, HOST, getUniq(), PRIVATE])
+
+            while (left.value() == 0) | (right.value() == 0):
+                time.sleep_ms(1)
+
+            right_assist = 0
+            right.irq(trigger=Pin.IRQ_FALLING, handler=right_handler)
+
+    right.irq(trigger=Pin.IRQ_FALLING, handler=right_handler)
+    down.irq(trigger=Pin.IRQ_FALLING, handler=down_handler)
+    timer.init(freq=1_000, mode=Timer.PERIODIC, callback=loop)
+
+
+class HDDActivityRecorder:
+    def __init__(self):
+        self.activity = [0 for _ in range(100)]
+
+    def record(self, value: int):
+        self.activity.pop(0)
+        self.activity.append(value)
+
+
+def processHostLEDs(state: State):
+    # print('Power LED', powerLED.value())
+    # print('HDD LED', hddLED.value())
+
+    # if powerLED.value() == 1:
+    #     display.powerOn()
+    # else:
+    #     display.powerOff()
+
+    state.HDDActivity.record(hddLED.value())
+
+
+def main():
+    LOOP_TIME = const(50)  # ms
+    LOOP_COUNTER = 0
+
+    state = State()
+    setupEncoder(state)
+    # setupEncoderV2(state)
+
+    lastLoopTime = LOOP_TIME
+    drift = 0
+    avgTime = LOOP_TIME
+
+    try:
+        uartHost = state.uartHost
+        uartESP = state.uartESP
+
+        uartHost.updateSysInfo()
+
+        while True:
+            loopStart = time_ns()
+
+            # blink led
+            if timeDivider(LOOP_COUNTER + 1, LOOP_TIME, 1_000):
+                picoLED.toggle()
+
+            uartHost.checkRX()
+            uartESP.checkRX()
+
+            if timeDivider(LOOP_COUNTER + 1, LOOP_TIME, 1_000):
+                if state.hostConnected:
+                    state.fetchSensors()
+                else:
+                    state.connectHost()
+
+            if timeDivider(LOOP_COUNTER, LOOP_TIME, 5_000):
+                state.fetchMQTTStatus()
+                # print(state.HDDActivity.activity)
+
+            if timeDivider(LOOP_COUNTER, LOOP_TIME, 20_000) and state.espConnected:
+                state.fetchWiFiSignal()
+
+            if timeDivider(LOOP_COUNTER, LOOP_TIME, 50):
+                uartHost.checkRequests()
+                uartESP.checkRequests()
+
+            if timeDivider(LOOP_COUNTER, LOOP_TIME, 100):
+                processHostLEDs(state)
+
+            # loop stuff
+            LOOP_COUNTER = (LOOP_COUNTER + 1) % 2_147_483_648
+
+            adjustedTime = min(LOOP_TIME * 2, max(0, LOOP_TIME - drift))
+            time.sleep_ms(adjustedTime)
+
+            lastLoopTime = math.ceil((time_ns() - loopStart) / 1_000_000)
+            lastLoopTime = max(0, lastLoopTime)  # handle overflow
+
+            avgTime = (avgTime * 49 + lastLoopTime) / 50
+            drift += lastLoopTime - LOOP_TIME
+
+            # if timeDivider(LOOP_COUNTER, LOOP_TIME, 5_000):
+            # print("loop time:", lastLoopTime, "avg:", avgTime, "drift:", drift)
+
+    except KeyboardInterrupt:
+        pass
+
+
+if (nextButton.value() == 0):
+    picoLED.on()
+else:
+    picoLED.off()
+    main()
